ea00d896aeccfaa2acae8ae879bdadf99c08ee29
Xiomie <mikegerxiong37@gmail.com>
2022-10-19 16:34:03 -0400

    Pause Menu

--- Assets/Scripts/PauseMenuScript.cs
+++ Assets/Scripts/PauseMenuScript.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class PauseMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject menu;
+    public TextMeshProUGUI testingText;
+    public KeyCode testingKey;
+    public bool isPaused = false;
+
     void Start()
     {
-        
+        Time.timeScale = 1f;
+        menu.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(testingKey))
+        {
+            testingText.text = "Down";
+        }
+        else if (Input.GetKey(testingKey))
+        {
+            testingText.text = "Hold";
+        }
+        else if (Input.GetKeyUp(testingKey))
+        {
+            testingText.text = "Up";
+        }
+        else
+        {
+        }
+
+        if (Input.GetButtonDown("Cancel"))
+        {
+            TogglePauseGame();
+        }
+    }
+
+    public void TogglePauseGame()
+    {
+        isPaused = !isPaused;
+        menu.SetActive(isPaused);
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
